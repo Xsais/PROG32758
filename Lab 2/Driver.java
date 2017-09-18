3e700a71b4fc462ff5accbd03ebc5a02217161f2
Nathaniel Primo <nathaniel.primo@gmail.com>
2017-09-18 14:27:09 -0400

    Changes from class

---
+++ Lab 2/Driver.java
@@ -0,0 +1,14 @@
+package L0_1_Pack;
+
+
+public class Driver {
+    /*****************************************************************
+    *                     Methods                                   *
+    *                                                               *
+    ****************************************************************/
+	//Method used by the Driver to accelerate the car
+     public void punchOnAccelorPedal(Car myCar, int accel)      
+	{
+    	myCar.accelerate(accel);
+	}  	
+}
