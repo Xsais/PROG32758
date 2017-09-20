8726cffd5391e185a7be6102a0401b7cdc43d5d3
Nathaniel Primo <nathaniel.primo@gmail.com>
2017-09-20 10:18:17 -0400

    Made SelfTriggerSiren interface and implemented it in Siren class

--- Assignment 1/src/com/g1/Siren.java
+++ Assignment 1/src/com/g1/Siren.java
@@ -21,7 +21,7 @@ package com.g1;
 
 import java.awt.*;
 
-public class Siren {
+public class Siren implements  SelfTriggerSiren {
 
     public void fireSiren() throws InterruptedException {
 
@@ -36,6 +36,21 @@ public class Siren {
 
                 Thread.sleep(1000);
             }
+        }
+    }
+
+    /**
+     * Starts the siren
+     */
+    @Override
+    public void TriggerSiren() {
+
+        try {
+
+            fireSiren();
+        } catch (InterruptedException e) {
+
+            e.printStackTrace();
         }
     }
 }
