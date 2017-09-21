f02d3b4abd4d9fe191045e913c168741ebc5db9a
Nathaniel Primo <nathaniel.primo@gmail.com>
2017-09-21 09:42:42 -0400

    Made ElectronicLifeSaving interface

---
+++ Assignment 1/src/com/g1/ElectronicLifeSaving.java
@@ -0,0 +1,36 @@
+/*
+ * ----------------------------------------------------------------------------+
+ * Group Leader: Daniel Hope
+ * Member(s): Georgina Luce
+ *            Nathaniel Primo
+ *            Michael Marc
+ * Group #: 1
+ * Filename: ElectronicLifeSaving.java
+ * Main class: 
+ * Other Files in this Project:
+ *     - 
+ * Assignment: 
+ * Creation Date: 09, 2017 21
+ * Last Modified: 09, 2017 21
+ * Java Version: 
+ * Description:
+ * ----------------------------------------------------------------------------+
+ */
+
+package com.g1;
+
+public interface ElectronicLifeSaving {
+
+    /**
+     * Detects when there has been an electronic crash
+     *
+     */
+    void CrashElectronicDetection();
+
+    /**
+     * Alerts the ambulance of a crash
+     *
+     */
+    void AlertAmbulanceManagementSystem();
+
+}
