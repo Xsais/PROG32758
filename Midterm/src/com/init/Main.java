49e5c4ce0e73b7a6518beeecfffff85741b5f9e7
Nathaniel Primo <nathaniel.primo@gmail.com>
2017-10-14 13:14:26 -0400

    Created starting point for the Micro-Project 1 (Part 1)/Midterm

---
+++ Midterm/src/com/init/Main.java
@@ -0,0 +1,57 @@
+/*
+ * ----------------------------------------------------------------------------+
+ * Group Leader: Daniel Hope
+ * Member(s): Georgina Luce
+ *            Nathaniel Primo
+ *            Michael Marc
+ * Group #: 1
+ * Filename: Main.java
+ * Main class: com.init.Main.java
+ * Other Files in this Project:
+ *     - com.views.StartPage.java
+ *     - com.views.StartPage.fxml
+ * Assignment: Midterm - Micro-Project 1 (Part 1)
+ * Creation Date: 10, 2017 14
+ * Last Modified: 10, 2017 14
+ * Java Version: 1.8.0_141
+ * Description:
+ * ----------------------------------------------------------------------------+
+ */
+
+package com.init;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.fxml.FXMLLoader;
+import java.io.IOException;
+
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        Scene scene = null;
+        try {
+
+            scene = new Scene(FXMLLoader.load(getClass().getResource("/com/views/StartPage.fxml")));
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+
+
+        primaryStage.setTitle("Car Racing Game");
+
+        //Specify the scene to be used for the stage a show the window.
+        primaryStage.setScene(scene);
+
+        primaryStage.show();
+    }
+}
---
+++ Midterm/src/com/views/StartPage.java
@@ -0,0 +1,39 @@
+/*
+ * ----------------------------------------------------------------------------+
+ * Group Leader: Daniel Hope
+ * Member(s): Georgina Luce
+ *            Nathaniel Primo
+ *            Michael Marc
+ * Group #: 1
+ * Filename: StartPage.java
+ * Main class: com.init.Main.java
+ * Other Files in this Project:
+ *     - com.views.StartPage.fxml
+ * Assignment: Midterm - Micro-Project 1 (Part 1)
+ * Creation Date: 10, 2017 14
+ * Last Modified: 10, 2017 14
+ * Java Version: 1.8.0_141
+ * Description:
+ * ----------------------------------------------------------------------------+
+ */
+
+package com.views;
+
+import javafx.fxml.Initializable;
+import java.util.ResourceBundle;
+import java.net.URL;
+
+public class StartPage implements Initializable {
+
+    /**
+     * Called to initialize a controller after its root element has been completely processed.
+     *
+     * @param location  The location used to resolve relative paths for the root object, or <tt>null</tt> if the
+     *                  location is not known.
+     * @param resources The resources used to localize the root object, or <tt>null</tt> if
+     */
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
