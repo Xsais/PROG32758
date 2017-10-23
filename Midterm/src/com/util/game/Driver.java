9144598c11b765d5e8d5ee398bc108aa17c0bcf7
Nathaniel Primo <nathaniel.primo@gmail.com>
2017-10-23 11:45:24 -0400

    Added files required for the game mechanics as provided

---
+++ Midterm/src/com/util/game/Driver.java
@@ -0,0 +1,15 @@
+package com.util.game;
+
+
+public class Driver {
+
+    /*****************************************************************
+     *                     Methods                                   *
+     *                                                               *
+     ****************************************************************/
+    //Method used by the Driver to accelerate the car
+    public void punchOnAccelorPedal(Car myCar, int accel) {
+
+        myCar.accelerate(accel);
+    }
+}
