9548cf5073a45d54432b616a1bb1e14834cda303
Nathaniel Primo <nathaniel.primo@gmail.com>
2017-10-23 19:34:47 -0400

    Added PageType

---
+++ Midterm/src/com/util/PageType.java
@@ -0,0 +1,26 @@
+/*
+ * ----------------------------------------------------------------------------+
+ * Group Leader: Daniel Hope
+ * Member(s): Georgina Luce
+ *            Nathaniel Primo
+ *            Michael Marc
+ * Group #: 1
+ * Filename: PageType.java
+ * Main class: 
+ * Other Files in this Project:
+ *     - 
+ * Assignment: 
+ * Creation Date: 10, 2017 21
+ * Last Modified: 10, 2017 21
+ * Java Version: 
+ * Description: The representation of a Car object
+ * ----------------------------------------------------------------------------+
+ */
+
+package com.util;
+
+public enum PageType {
+
+    Page,
+    Popout
+}
