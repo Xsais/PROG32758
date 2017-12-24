cd4673972b2f3d9a6144255187d4d9ead7a1c993
Dan Hope <32016873+hopedani@users.noreply.github.com>
2017-12-24 16:50:04 -0500

    Changed everything so that it works

--- Final Exam/src/com/utils/helperclasses/UserLoginVerification.java
+++ Final Exam/src/com/utils/helperclasses/UserLoginVerification.java
@@ -1,142 +1,152 @@
+/*
+
+
+
+ * ----------------------------------------------------------------------------------------------+
+
+
+
+ *  * Group Leader: Daniel Hope
+
+
+
+ *  * Member(s):
+
+
+
+ *  *     - Georgina Luce
+
+
+
+ *  *     - Nathaniel Primo
+
+
+
+ *  *     - Michael Marc
+
+
+
+ *  * Group #: 1
+
+
+
+ *  * Filename: com.utils.helperClasses.UserLoginVerification
+
+
+
+ *  * Assignment: Final Exam
+
+
+
+ *  * Creation Date:
+
+
+
+ *  * Last Modified: 12/24/17 4:37 PM
+
+
+
+ *  * Java Version: 1.8.0_141
+
+
+
+ *  * Description: 
+ *  Helper class that keeps track of user attempts by checking DBProg32758 and verifying that login and password credentials
+ *  are correct, if not tracking lists are updated and control is returned to UserLoginServlet. In the case of 3 consecutive failed 
+ *  attempts at a user login the database is updated and the user is locked out from any further attempts.
+
+
+
+ * ----------------------------------------------------------------------------------------------+
+
+
+
+ */
+
 package com.utils.helperclasses;
 
-import java.awt.HeadlessException;
 import java.sql.ResultSet;
+
 import java.sql.SQLException;
 
-import javax.swing.JOptionPane;
+import com.utils.servlets.UserLoginServlet;
 
 public class UserLoginVerification {
-	
-	public UserLoginVerification() { }
 
 	private static final String LOCK_OUT_CODE = "xxxxxx";
 
-	private int failCount = 0;
+	private static int failCount = 0;
 
-	private ResultSet rs = null;
+	private static ResultSet rs = null;
 
-	private ConnectToDB dbConnection;
+	public static boolean isValidLogin(String login, String password, Object dbconnection) throws SQLException {
 
-	public boolean isValidLogin(String login, String password) throws SQLException {
-		
-		if (failCount < 3) {
+		// make connection to DBProg32758 in order to check and update login credentials
+		ConnectToDB dbConnection = (ConnectToDB) dbconnection;
 
-			// check existence of user login
+		try {
 
-			try {
+			// check DB for user defined login
 
-				// check DB for user defined login
+			rs = dbConnection.executeQuerry(String.format("SELECT * FROM DBProg32758.Players WHERE Login = '%s'"
 
-				rs = dbConnection.executeQuerry(String.format("SELECT * FROM DBProg32758.Players WHERE Login = '%s'"
+					, login));
 
-						, login));
+		} catch (SQLException e) {
 
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
+			e.printStackTrace();
 
-			if (rs.next()) {
+		}
 
-				// if login is valid, check if user account is already locked
+		if (rs.next()) {
 
-				if (rs.getString("Password").equals(LOCK_OUT_CODE)) {
+			// check if user is already in the list of logins, if not add them and
+			// initialize attempts count
+			if (!UserLoginServlet.loginTracker.contains(login)) {
 
-					showLockOutMessage(login);
+				UserLoginServlet.loginTracker.add(login);
+				UserLoginServlet.attemptsTracker.add(1);
 
-					failCount = 3;
-
-					// if login is valid, check password validity associated with login
-
-				} else if (rs.getString("Password").equals(password)) {
-
-					failCount = 0;
-
-					return true;
-
-				} else {
-
-					showLoginErrorMessage();
-
-					if (failCount == 3) {
-
-						showLockOutMessage(login);
-
-					}
-
-				}
-
-				// inform user that login and/or password are incorrect (login incorrect
-				// scenario)
-
+				// assign number of attempts from attemptsTracker list and store in failCount to check validity
+				// of login credentials
 			} else {
 
-				showLoginErrorMessage();
+				failCount = UserLoginServlet.attemptsTracker.get(UserLoginServlet.loginTracker.indexOf(login));
+			}
+			// if login is valid, check if user account is already locked
+			if (rs.getString("Password").equals(LOCK_OUT_CODE)) {
+
+				UserLoginServlet.attemptsTracker.set(UserLoginServlet.loginTracker.indexOf(login), 3);
+
+				return false;
+
+				// if login is valid, check password validity associated with login
+			} else if (rs.getString("Password").equals(password)) {
+
+				UserLoginServlet.attemptsTracker.set(UserLoginServlet.loginTracker.indexOf(login), 0);
+
+				return true;
+				// add to attemptTracker and if 3 failed attempts in a row, update DB to lock user out
+			} else {
+				
+				failCount += 1;
+
+				UserLoginServlet.attemptsTracker.set(UserLoginServlet.loginTracker.indexOf(login), failCount);
 
 				if (failCount == 3) {
+					dbConnection.executeUpdate(
 
-					showLockOutMessage(login);
+							String.format("UPDATE DBProg32758.Players SET Password = 'xxxxxx' WHERE Login = '%s'",
+									login));
 
 				}
+				return false;
 
 			}
 
 		} else {
 
-			showLockOutMessage(login);
-
+			return false;
 		}
-
-		return false;
-	}
-	
-	/**
-	 * 
-	 * Inform user that their account has been locked, locks the account, and exits
-	 * login pop out window
-	 * 
-	 */
-	private void showLockOutMessage(String login) throws SQLException {
-
-		// inform user that account has been locked
-
-		JOptionPane.showMessageDialog(null, "Your account has been locked\n Please contact your Database Administrator",
-
-				"Car Racing Game", JOptionPane.INFORMATION_MESSAGE);
-
-		// set user password to all x's in DB
-
-		dbConnection.executeUpdate(
-
-				String.format("UPDATE DBProg32758.Players SET Password = 'xxxxxx' WHERE Login = '%s'", login));
-
-		// exit login screen
-
-		//btnExit.fire();
-
-	}
-	
-	/**
-	 * 
-	 * Inform user of incorrect login/password and increments failed attempts
-	 * counter by one
-	 * 
-	 */
-	private void showLoginErrorMessage() {
-
-		// inform user of incorrect login/password
-
-		JOptionPane.showMessageDialog(null,
-
-				"The Login and Password are not correct\n Please try again"
-
-						+ " if you are already registered or complete your registration if you are not",
-
-				"Car Racing Game", JOptionPane.INFORMATION_MESSAGE);
-
-		// increase fail counter by 1
-
-		failCount += 1;
-
 	}
 }
