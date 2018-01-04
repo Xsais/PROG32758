24f66bdd770a3ebe881b805784dba130ede8c655
Dan Hope <32016873+hopedani@users.noreply.github.com>
2018-01-04 00:50:34 -0500

    Added error handling

--- Assignment 3/src/myServlets/MagicSquareServlet.java
+++ Assignment 3/src/myServlets/MagicSquareServlet.java
@@ -15,57 +15,63 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/MagicSquareServlet")
 public class MagicSquareServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-     
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public MagicSquareServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		
-		
-		// variable contains user size preference for magic square
-		int userPref = Integer.parseInt(request.getParameter("magicSquareSize"));
-		
-		// pass user defined matrix size to helper class to create Magic Square
-		int[][] magicSquare = MagicSquareHelper.createMagicSquare(userPref);
-		
-		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
-		
-		out.println("<!DOCTYPE html>\r\n" + 
-				"<html>\r\n" + 
-				"<head>\r\n" + 
-				"<meta charset=\"ISO-8859-1\">\r\n" + 
-				"<title>Assignment 3</title>\r\n" + 
-				"</head>\r\n" + 
-				"<body><h1>Assignment 3 by Group 1</h1>\r\n" + 
-				"	<br><form action=\"MagicSquareServlet\" method=\"GET\">\r\n" + 
-				"		Enter size of Magic Square: <input type=\"text\" name='magicSquareSize'>\r\n" + 
-				"		<br> \r\n" + 
-				"		<input type=\"submit\" value=\"Enter\">\r\n" + 
-				"	</form><br><table border=1>");
-				for(int i = 0; i < userPref; i++) {
-					out.println("<tr>");
-					for(int j = 0; j < userPref; j++) {
-						out.printf("<td>" + magicSquare[i][j] + "</td>");
-					}
-				} 
-				out.println("</table></body></html>");
-	
+	public MagicSquareServlet() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+
+		// variable contains user size preference for magic square
+		int userPref = Integer.parseInt(request.getParameter("magicSquareSize"));
+
+		// selection statement used to prevent errors, magic box must be at least 2x2 in
+		// size to work
+		if (userPref < 2) {
+			out.println("<!DOCTYPE html>\r\n" + "<html>\r\n" + "<head>\r\n" + "<meta charset=\"ISO-8859-1\">\r\n"
+					+ "<title>Assignment 3</title>\r\n" + "</head>\r\n" + "<body><h1>Assignment 3 by Group 1</h1>\r\n"
+					+ "	<br><form action=\"MagicSquareServlet\" method=\"GET\">\r\n"
+					+ "		Enter size of Magic Square: <input type=\"text\" autofocus pattern='\\d{1,}' title='Enter a number' name='magicSquareSize' required>\r\n"
+					+ "		<br> \r\n" + "		<input type=\"submit\" value=\"Enter\">\r\n" + "	</form><br>"
+					+ "<script>alert('You must enter a number higher than 1')</script>" + "</body></html");
+		} else {
+			// pass user defined matrix size to helper class to create Magic Square
+			int[][] magicSquare = MagicSquareHelper.createMagicSquare(userPref);
+
+			out.println("<!DOCTYPE html>\r\n" + "<html>\r\n" + "<head>\r\n" + "<meta charset=\"ISO-8859-1\">\r\n"
+					+ "<title>Assignment 3</title>\r\n" + "</head>\r\n" + "<body><h1>Assignment 3 by Group 1</h1>\r\n"
+					+ "	<br><form action=\"MagicSquareServlet\" method=\"GET\">\r\n"
+					+ "		Enter size of Magic Square: <input type=\"text\" autofocus pattern='\\d{1,}' title='Enter a number' name='magicSquareSize' required>\r\n"
+					+ "		<br> \r\n" + "		<input type=\"submit\" value=\"Enter\">\r\n"
+					+ "	</form><br><table border=1>");
+			for (int i = 0; i < userPref; i++) {
+				out.println("<tr>");
+				for (int j = 0; j < userPref; j++) {
+					out.printf("<td>" + magicSquare[i][j] + "</td>");
+				}
+			}
+			out.println("</table></body></html>");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
